327aa4890cc1e850b3e61294f2f8aff205c8a4d6
nesoni1 <maglovskinesa@gmail.com>
2023-09-27 21:51:45 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/navbar'
+import nenad from './image/nenad.jpg'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Radi samo da znas.
+        </p>
+        <div><img src={nenad} className='nenad'/></div>
+      </header>
+     
+    </div>
+  );
+}
+
+export default App;
